70b1433274ce9239e02792914fd346bf0fcf9d8c
jiny <jin03083@ajou.ac.kr>
2021-04-06 08:32:31 +0900

    working on score method

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -28,7 +28,7 @@ public class BowlingGameTest {
 		assertEquals(20, g.score());
 	}
 	
-	@Ignore("until we get design right")
+	@Ignore("until we walk frame by frame")
 	@Test
 	public void testOneSpare() {
 		g.roll(5);
