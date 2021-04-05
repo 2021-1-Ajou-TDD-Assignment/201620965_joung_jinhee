69bf820d9b6e3f727dfa0ab849a8b91a8dfcbaae
jiny <jin03083@ajou.ac.kr>
2021-04-06 08:53:44 +0900

    perfect game test / two strikes test added

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -45,6 +45,20 @@ public class BowlingGameTest {
 		assertEquals(24,g.score());
 	}
 	
+	@Test
+	public void testPerfectGame() {
+		rollMany(12,10);
+		assertEquals(300, g.score());
+	}
+	
+	@Test
+	public void testTwoStrikes() {
+		rollStrike();
+		rollStrike();
+		rollMany(0,16);
+		assertEquals(30,g.score());
+	}
+	
 	private void rollSpare() {
 		g.roll(5);
 		g.roll(5);
